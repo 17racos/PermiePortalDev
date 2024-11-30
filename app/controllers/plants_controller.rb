b166e63d452670b916eb878f52ffa6ebe1733483
17racos <17racos@gmail.com>
2024-11-30 09:23:48 -0500

    Added first iteration of Plant filtering

--- app/controllers/plants_controller.rb
+++ app/controllers/plants_controller.rb
@@ -1,9 +1,17 @@
 class PlantsController < ApplicationController
   def index
     @plants = Plant.all
+    @plants = apply_filters(@plants)
   end
 
-  def show
-    @plant = Plant.find(params[:id]) # Fetch the plant using its ID
+  private
+
+  def apply_filters(plants)
+    if params[:search].present?
+      plants = plants.filter_by_functions(params[:search][:functions]) if params[:search][:functions].present?
+      plants = plants.filter_by_layers(params[:search][:layers]) if params[:search][:layers].present?
+      plants = plants.filter_by_zones(params[:search][:zones]) if params[:search][:zones].present?
+    end
+    plants
   end
 end
--- app/models/plant.rb
+++ app/models/plant.rb
@@ -1,2 +1,34 @@
 class Plant < ApplicationRecord
+  # === Validations ===
+  validates :common_name, :scientific_name, :zone, :layers, :function, presence: true
+  validates :perennial, inclusion: { in: [true, false] }
+
+  # === Scopes ===
+  scope :filter_by_functions, ->(functions) { where("function ILIKE ANY (array[?])", functions.map { |f| "%#{f}%" }) if functions.present? }
+  scope :filter_by_layers, ->(layers) { where("layers ILIKE ANY (array[?])", layers.map { |l| "%#{l}%" }) if layers.present? }
+  scope :filter_by_zones, ->(zones) { where(zone: zones) if zones.present? }
+
+  # === Instance Methods ===
+
+  # Returns "Perennial" or "Annual" for easier display in views
+  def type
+    perennial ? "Perennial" : "Annual"
+  end
+
+  # Splits layers into an array for easier iteration
+  def layers_array
+    split_field(layers)
+  end
+
+  # Splits functions into an array for easier iteration
+  def functions_array
+    split_field(function)
+  end
+
+  private
+
+  # Helper to split a string into an array, trimming whitespace
+  def split_field(field)
+    field.split(",").map(&:strip) if field.present?
+  end
 end
