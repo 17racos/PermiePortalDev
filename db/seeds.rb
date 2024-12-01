82a624a7d07300952cf4fd96de64d61ac78bd9cc
17racos <17racos@gmail.com>
2024-11-30 21:58:10 -0500

    added additional fields and data to plant model

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,15 +5,21 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+Plant.find_or_create_by!(common_name: "Comfrey") do |plant|
 
-Plant.find_or_create_by!(common_name: "Rosemary") do |plant|
-  plant.picture = "rosemary.png"
-  plant.common_name = "Rosemary"
-  plant.scientific_name = "Rosmarinus officinalis"
-  plant.family = "Lamiaceae"
-  plant.zone = "5-9"
-  plant.perennial = true
-  plant.layers = ["Ground Cover", "Herbaceous"]
-  plant.function = ["Companion Planting", "Pest Deterrent","Medicinal", "Culinary", "Soil Health", "Ground Cover", "Windbreak", "Microclimate Creation"]
-  plant.purpose = "Rosemary is used for companion planting to deter pests, improve soil health, and provide medicinal and culinary uses. It is drought-tolerant and helps with erosion control. As a perennial, it reduces water usage and maintenance. It also serves as a windbreak and can create favorable microclimates in permaculture systems."
+  plant.layers = ["Herbaceous"]
+  plant.function = ["Medicinal", "Dynamic Accumulator", "Mulcher"]
+
+  # Assigning the multi-line purpose text
+  plant.purpose = <<-PURPOSE
+    Comfrey is an incredibly versatile and beneficial plant for permaculture systems. Its ability to improve soil health, accumulate nutrients, support biodiversity, and provide fodder and medicinal benefits make it an essential component of sustainable farming and gardening practices.
+    - Soil Improvement: Comfrey's deep roots access nutrients from deep within the soil and enrich the topsoil when its leaves decompose.
+    - Dynamic Accumulator: Comfrey accumulates potassium, calcium, and other minerals, enriching the soil for surrounding plants.
+    - Compost Activator: Comfrey accelerates composting due to its high nitrogen content.
+    - Mulching Material: Comfrey leaves are an excellent mulch that retains moisture, suppresses weeds, and adds nutrients to the soil.
+    - Animal Fodder: Comfrey is highly nutritious and can be used to supplement animal diets.
+    - Medicinal Uses: Known for promoting cell regeneration, comfrey has been used to treat wounds, bruises, and inflammation.
+    - Biodiversity Support: Comfrey attracts pollinators, improving biodiversity in the garden.
+    - Low Maintenance: Once established, comfrey requires little maintenance and thrives in various soil conditions.
+  PURPOSE
 end
