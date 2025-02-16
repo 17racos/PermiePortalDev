3308b7e433ad52028fc0ff12f53182c76614fe42
17racos <17racos@gmail.com>
2025-02-16 16:03:12 +0000

    Dev enviornment down to minimal errors still need to work on css

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,10 +1,3 @@
-// Import necessary dependencies
-import { Turbo } from "@hotwired/turbo-rails";
-import * as ActiveStorage from "@rails/activestorage";
-import "controllers";  // Only if using Stimulus (via stimulus-rails)
-
-// Start ActiveStorage if you're using file uploads
-ActiveStorage.start();
-
-// Log Turbo to check if it's working
-console.log("Turbo is running:", Turbo);
+//= require rails-ujs
+//= require activestorage
+//= require_tree .
